a97b039a52fd4233224579ad2da3aedeafae2579
Rajan1902 <82579513+Rajan1902@users.noreply.github.com>
2023-02-01 19:22:58 +0530

    2nd commit

---
+++ index.js
@@ -0,0 +1,56 @@
+var buttonNum = document.querySelectorAll(".drum").length;
+for(var i = 0; i<buttonNum; i++){
+    document.querySelectorAll(".drum")[i].addEventListener("click", function(){
+        var buttonName = this.innerHTML;
+        sound(buttonName);
+        animate(buttonName);
+    });
+}
+    document.addEventListener("keydown", function(e){
+        sound(e.key);
+        animate(e.key);
+    });
+
+    function sound(key){
+        switch (key) {
+            case "w":
+                var tom4 = new Audio("sounds/tom-4.mp3");
+                tom4.play();
+                break;
+            case "a":
+                var tom3 = new Audio("sounds/tom-3.mp3");
+                tom3.play();
+                break;
+            case "s":
+                var tom2 = new Audio("sounds/tom-2.mp3");
+                tom2.play();
+                break;
+            case "d":
+                var tom1 = new Audio("sounds/tom-1.mp3");
+                tom1.play();
+                break;
+            case "j":
+                var snare = new Audio("sounds/snare.mp3");
+                snare.play();
+                break;
+            case "k":
+                var kick = new Audio("sounds/kick-bass.mp3");
+                kick.play();
+                break;
+            case "l":
+                var crash = new Audio("sounds/crash.mp3");
+                crash.play();
+                break;
+        
+            default:
+                break;
+        }
+    }
+    function animate(keyPressed) {
+        var temp = document.querySelector("."+keyPressed);
+        temp.classList.add("pressed");
+        setTimeout(function(){
+            temp.classList.remove("pressed");
+        }, 100);
+    }
+
